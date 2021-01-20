ffb1bf5baee9e025e37268e2b833945b2f3d835f
Cristiano Ressi di Cervia <cristiano.ressidicervia@gmail.com>
2021-01-20 09:43:59 +0100

    web hook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.println("MY JENKINSSS IN SYNC 2");
+        System.out.println("MY JENKINSSS IN SYNC WEB HOOK");
     }
 }
